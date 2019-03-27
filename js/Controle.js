b49b9f7aea121cc3b4b5276aed3df40436f4fa60
aymenkefi <aymenkefi1919@gmail.com>
2019-03-27 02:45:20 +0100

    exerci 1 et 2

--- js/Controle.js
+++ js/Controle.js
@@ -1,1 +1,16 @@
-alert('Hellow World');
+var n = Math.round(Math.random() * 100);
+var ch = "yes";
+
+do {
+    var i = 1;
+    var x = prompt("HEYYY§ GUESS THE NUMBER [1..100]  ", "");
+    do {
+        i=i+1;
+        if (x==null) i=9;
+        else{
+        if (x > n) var x = prompt("YOUR NUMBER > TRY AGAIN ", "");
+        if (x < n) var x = prompt("YOUR NUMBER < TRY AGAIN ", "");}
+    } while ((i < 8) && (x != n));
+    if (x == n) var ch = prompt("BRAVO ! WONNA PLAY AGAIN", "");
+    else var ch = prompt("KHSARA ! WONNA PLAY AGAIN", "");
+} while (ch == "yes");
